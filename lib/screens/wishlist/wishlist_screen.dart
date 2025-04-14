65cff831c0d666a2bb508b8d8adb184ed5290d3a
Fawas Surajudeen <fawassurajudeen16@gmail.com>
2025-04-14 11:45:06 +0100

    third project

--- lib/screens/wishlist/wishlist_screen.dart
+++ lib/screens/wishlist/wishlist_screen.dart
@@ -6,43 +6,53 @@ import 'package:provider/provider.dart';
 class WishlistScreen extends StatelessWidget {
   const WishlistScreen({super.key});
 
-  // In wishlist_screen.dart
   @override
   Widget build(BuildContext context) {
     final wishlist = Provider.of<BookService>(context).wishlist;
 
     return Scaffold(
       appBar: AppBar(title: const Text('My Wishlist')),
-      body: SingleChildScrollView(
-        // Add this
-        child: ConstrainedBox(
-          // And this
-          constraints: BoxConstraints(
-            minHeight: MediaQuery.of(context).size.height,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bg2.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.9),
+              BlendMode.dstATop,
+            ),
           ),
-          child: wishlist.isEmpty
-              ? Center(
-                  child: Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Text('Your wishlist is empty'),
+        ),
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: MediaQuery.of(context).size.height,
+            ),
+            child: wishlist.isEmpty
+                ? Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Text('Your wishlist is empty'),
+                    ),
+                  )
+                : GridView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    padding: const EdgeInsets.all(16),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.7,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                    ),
+                    itemCount: wishlist.length,
+                    itemBuilder: (context, index) => BookCard(
+                      book: wishlist[index],
+                      onTap: () {/* Navigation */},
+                    ),
                   ),
-                )
-              : GridView.builder(
-                  shrinkWrap: true, // Add this
-                  physics: NeverScrollableScrollPhysics(), // Add this
-                  padding: const EdgeInsets.all(16),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 0.7,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                  ),
-                  itemCount: wishlist.length,
-                  itemBuilder: (context, index) => BookCard(
-                    book: wishlist[index],
-                    onTap: () {/* Navigation */},
-                  ),
-                ),
+          ),
         ),
       ),
     );
